6c7cf16d8ff9a0139105e91687542530d02cc9dd
Lehduq <dqle123@gmail.com>
2021-04-10 19:49:54 -0700

    Recreate Server in Python

--- go.mod
+++
@@ -1,9 +0,0 @@
-module example.com/m/v2
-
-go 1.16
-
-require (
-	github.com/gorilla/mux v1.8.0
-	github.com/icco/scroll-phat-hd-go v0.2.1
-	periph.io/x/periph v3.6.7+incompatible
-)
---
+++ pi-busylight-server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import signal
+import time
+import scrollphathd
+from flask import Flask
+import logging
+
+app = Flask(__name__)
+
+#Disable logging
+log = logging.getLogger('werkzeug')
+log.disabled = True
+app.logger.disabled = True
+
+
+@app.route('/')
+def home():
+    return "pi-busylight is running"
+
+@app.route('/api/on', methods=['POST'])
+def busylight_on():
+    scrollphathd.fill(brightness=0.5)
+    scrollphathd.show()
+    return "on"
+
+@app.route('/api/off', methods=['POST'])
+def busylight_off():
+    scrollphathd.clear()
+    scrollphathd.show()
+    return "off"
+
+if __name__ == '__main__':
+    app.run()
